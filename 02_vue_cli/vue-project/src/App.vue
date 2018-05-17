8768b436a68757168b0a3434f43d396390812f69
Pavel <triodjangopiter@yandex.ru>
2018-05-17 13:30:44 +0300

    data transfer between components

--- 02_vue_cli/vue-project/src/App.vue
+++ 02_vue_cli/vue-project/src/App.vue
@@ -1,7 +1,19 @@
 <template>
     <div>
         <app-car></app-car>
-        <app-person></app-person>
+        <hr>
+        <h1>{{ firstName }}</h1>
+        <app-person
+            :firstName="firstName"
+            :lastName="lastName"
+            :counter="counter"
+            :changeFunc="changeNameToPavel"
+            @eventName="firstName = $event"
+            @counterUpdated="counter = $event"
+        >
+        </app-person>
+        <app-counter :counter="counter"></app-counter>
+
     </div>
 </template>
 
@@ -10,10 +22,27 @@
      * Локальная регистрация компонента.
      */
     import Person from './Person.vue';
+    import Counter from './Counter.vue';
 
     export default {
+        data() {
+            return {
+                firstName: 'Pavel',
+                lastName: 'Ravvich',
+                counter: 0
+            }
+        },
         components: {
-            appPerson: Person
+            appPerson: Person,
+            appCounter: Counter
+        },
+        methods: {
+            changeNameToPavel() {
+                this.firstName = 'Pavel'
+            },
+            updateCounter() {
+                this.$emit('counterUpdated', this.counter + 1)
+            }
         }
     }
 </script>
--- 02_vue_cli/vue-project/src/Person.vue
+++ 02_vue_cli/vue-project/src/Person.vue
@@ -1,16 +1,41 @@
 <template>
     <div>
-        <h3>First name: {{ firstName }}</h3>
+        <h3>First name: {{ firstName }} \ Revers : {{ reversStr }}</h3>
         <p>Last name: {{ lastName }}</p>
+        <button @click="changeName">Change name</button>
+        <button @click="changeFunc">Change from parent</button>
+        <button @click="updateCounter">Update counter</button>
     </div>
 </template>
 
 <script>
     export default {
-        data() {
-            return {
-                firstName: 'Pavel',
-                lastName: 'Ravvich'
+        /**
+         * Три формата для описания ожидаемых значений в props.
+         */
+        props: ['firstName', 'lastName', 'changeFunc', 'counter'],
+        // props: {
+        //     firstName: String
+        // },
+        // props: {
+        //     firstName: {
+        //         type: String,
+        //         required: true,
+        //         default: 'Default value'
+        //     }
+        // },
+        computed: {
+            reversStr() {
+                return this.firstName.split('').reverse().join('');
+            }
+        },
+        methods:{
+            changeName() {
+                this.firstName = 'new value';
+                this.$emit('eventName', this.firstName);
+            },
+            updateCounter() {
+                this.$emit('counterUpdated', this.counter + 1);
             }
         }
     }
