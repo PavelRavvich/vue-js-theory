8703d0aff7900fc44cacee8a8a05e2c860e29565
Pavel <triodjangopiter@yandex.ru>
2018-05-20 22:15:47 +0300

    add directives course module

---
+++ 03_directive/vue-project/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h2>{{title}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Hello!'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
