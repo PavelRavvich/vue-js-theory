23ecdf38bdd4ff6aba45d43c9b03abe386da3d6e
Pavel <triodjangopiter@yandex.ru>
2018-05-27 14:53:45 +0300

    add base stucture for vuex

---
+++ 09_vuex/vue-project/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" class="container text-center pt-5">
+    <app-counter :counter="counter"></app-counter>
+    <app-actions @counterUpdated="counter += $event"></app-actions>
+  </div>
+</template>
+
+<script>
+  import Counter from './Counter.vue'
+  import Actions from './Actions.vue'
+  export default {
+    name: 'app',
+    data () {
+      return {
+        counter: 0
+      }
+    },
+    components: {
+      appCounter: Counter,
+      appActions: Actions
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ 09_vuex/vue-project/src/Counter.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>Counter {{counter}}</h2>
+</template>
+
+<script>
+    export default {
+        props: ['counter']
+    }
+</script>
+
+<style scoped>
+</style>
